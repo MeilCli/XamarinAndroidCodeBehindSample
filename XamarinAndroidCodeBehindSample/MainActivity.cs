3c2f06d25fe98c65ee64493885eb4839fdbbb901
meil <ics.sora.meil@gmail.com>
2018-11-08 09:39:16 +0900

    プロジェクト ファイルを追加します。

---
+++ XamarinAndroidCodeBehindSample/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using System;
+
+namespace XamarinAndroidCodeBehindSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public MainActivity() { }
+
+        public MainActivity(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ActivityMain);
+
+            var binding = new Binding.ActivityMain(this);
+
+            SetSupportActionBar(binding.Toolbar);
+
+            binding.FloatingActionButton.Click += fabClicked;
+        }
+
+        private void fabClicked(object sender, EventArgs eventArgs)
+        {
+            StartActivity(new Intent(this, typeof(SubActivity)));
+        }
+    }
+}
+
---
+++ XamarinAndroidCodeBehindSample/SubActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using System;
+
+namespace XamarinAndroidCodeBehindSample
+{
+    [Activity]
+    public partial class SubActivity : AppCompatActivity
+    {
+        public SubActivity() { }
+
+        public SubActivity(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ActivitySub);
+
+            Message.Text = "Hello World";
+        }
+    }
+}
